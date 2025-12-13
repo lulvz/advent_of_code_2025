e640fe706714bb4b1092c4a10e7207e41a4d46d7
lulvz <kekwm8@gmail.com>
2025-12-13 15:28:33 +0000

    Added start for day 8

---
+++ src/bin/day08.rs
@@ -0,0 +1,106 @@
+use advent_of_code_2025::utils::{self, Vector};
+use std::array;
+
+fn part_one(input: &str) -> u64 {
+    // We are given a list of junction box positions, in the format 1,2,3\n4,5,6
+    // We have to find the junction boxes that are closer together in a straight line
+    // After connecting the two closest junction boxes, they form a circuit, then
+    // we have to find the next two closest boxes, and if one of them belongs to a
+    // circuit already, then that circuit gets a new box
+    // We have to return the multilication of the 3 largest circuit sizes
+    // basically im gonna slap all the points on a vector of f32 just to read the
+    // values into memory, then I'm gonna iterate through the vector and calculate
+    // the distances between every pair (I don't exactly know how I would represent
+    // this in memory though), then I order those distances, and finally I take the
+    // top 1000 distances and make the circuits
+    let mut jbox_positions: Vec<Vector<u32, 3>> = Vec::new();
+    for l in input.lines() {
+        let mut parts = l.split(',').map(|s| s.trim().parse::<u32>().unwrap());
+        jbox_positions.push(array::from_fn(|_| parts.next().unwrap()).into());
+    }
+    println!("{:?}", jbox_positions);
+
+    let mut v1: Vector<u32, 3> = Vector::from([1, 2, 3]);
+    v1 += [1, 1, 1].into();
+    println!("{:?}", v1);
+    32
+}
+
+fn part_two(_input: &str) -> u64 {
+    32
+}
+
+fn main() {
+    match utils::read_input("day08.txt") {
+        Ok(input) => {
+            println!("Day 08 - Part 1: {}", part_one(input.as_str()));
+            println!("Day 08 - Part 2: {}", part_two(input.as_str()));
+        }
+        Err(e) => {
+            eprintln!("Error reading input file: {}", e);
+            eprintln!("Make sure 'inputs/day08.txt' exists.");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_STRING: &str = "162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689";
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part_one(EXAMPLE_STRING), 40);
+    }
+
+    // #[test]
+    // fn test_part1_simple() {
+    //     assert_eq!(part_one("987654321111111"), 98);
+    // }
+
+    // #[test]
+    // fn test_part2_example() {
+    //     assert_eq!(part_two(EXAMPLE_STRING), 40);
+    // }
+    //
+    // #[test]
+    // fn test_part2_alt_example() {
+    //     assert_eq!(part_two_alt(EXAMPLE_STRING), 3263827);
+    // }
+    //
+    // #[test]
+    // fn test_part2_simple() {
+    //     assert_eq!(part_two("1 3\n4 6\n+ + "), 1);
+    // }
+
+    // #[test]
+    // fn test_part2_simple_second() {
+    //     assert_eq!(part_two("811111111111119"), 811111111119);
+    // }
+    // #[test]
+    // fn test_bad_add() {
+    //     // This assert would fire and test will fail.
+    //     // Please note, that private functions can be tested too!
+    //     assert_eq!(bad_add(1, 2), 3);
+    // }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::Path;
-
+use std::{
+    fmt::Debug,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
+};
 pub mod utils {
+    use std::array;
+
     use super::*;
 
     pub fn read_input(filename: &str) -> io::Result<String> {
@@ -10,7 +15,188 @@ pub mod utils {
         let mut file = File::open(path)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        
-        Ok(contents.to_string()) 
+
+        Ok(contents.to_string())
+    }
+
+    pub trait Scalar:
+        Sized
+        + Default
+        + Copy
+        + Clone
+        + Debug
+        + Add<Output = Self>
+        + Sub<Output = Self>
+        + Mul<Output = Self>
+        + Div<Output = Self>
+        + AddAssign
+        + SubAssign
+        + MulAssign
+        + DivAssign
+    {
+    }
+
+    // this can be interpreted as: implement the trait Scalar for all the types (T) that ipmlement
+    // all those other traits down there
+    impl<T> Scalar for T where
+        T: Sized
+            + Default
+            + Copy
+            + Clone
+            + Debug
+            + Add<Output = Self>
+            + Sub<Output = Self>
+            + Mul<Output = Self>
+            + Div<Output = Self>
+            + AddAssign
+            + SubAssign
+            + MulAssign
+            + DivAssign
+    {
+    }
+
+    // ////////////// VECTOR STRUCT /////////////////////
+    #[derive(Debug)]
+    pub struct Vector<T: Scalar, const N: usize> {
+        data: [T; N],
+    }
+
+    impl<T: Scalar, const N: usize> From<[T; N]> for Vector<T, N> {
+        fn from(data: [T; N]) -> Self {
+            Vector { data }
+        }
+    }
+
+    // ////////////// OPERATOR IMPLEMENTATIONS /////////////////////
+    impl<T: Scalar, const N: usize> Add for Vector<T, N> {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            array::from_fn(|i| self.data[i] + rhs.data[i]).into()
+        }
+    }
+
+    impl<T: Scalar, const N: usize> AddAssign for Vector<T, N> {
+        fn add_assign(&mut self, rhs: Self) {
+            for i in 0..N {
+                self.data[i] += rhs.data[i];
+            }
+        }
+    }
+
+    impl<T: Scalar, const N: usize> Sub for Vector<T, N> {
+        type Output = Self;
+
+        fn sub(self, rhs: Self) -> Self::Output {
+            array::from_fn(|i| self.data[i] - rhs.data[i]).into()
+        }
+    }
+
+    impl<T: Scalar, const N: usize> SubAssign for Vector<T, N> {
+        fn sub_assign(&mut self, rhs: Self) {
+            for i in 0..N {
+                self.data[i] -= rhs.data[i];
+            }
+        }
+    }
+
+    impl<T: Scalar, const N: usize> Mul for Vector<T, N> {
+        type Output = Self;
+
+        fn mul(self, rhs: Self) -> Self::Output {
+            array::from_fn(|i| self.data[i] * rhs.data[i]).into()
+        }
+    }
+
+    impl<T: Scalar, const N: usize> MulAssign for Vector<T, N> {
+        fn mul_assign(&mut self, rhs: Self) {
+            for i in 0..N {
+                self.data[i] *= rhs.data[i];
+            }
+        }
+    }
+
+    impl<T: Scalar, const N: usize> Div for Vector<T, N> {
+        type Output = Self;
+
+        fn div(self, rhs: Self) -> Self::Output {
+            array::from_fn(|i| self.data[i] / rhs.data[i]).into()
+        }
+    }
+
+    impl<T: Scalar, const N: usize> DivAssign for Vector<T, N> {
+        fn div_assign(&mut self, rhs: Self) {
+            for i in 0..N {
+                self.data[i] /= rhs.data[i];
+            }
+        }
+    }
+    // ////////////// CUSTOM OPERATOR TRAITS /////////////////////
+    pub trait Dot {
+        type Scalar;
+        fn dot(self, rhs: Self) -> Self::Scalar;
+    }
+
+    // ////////////// CUSTOM OPERATOR IMPLEMENTATIONS /////////////////////
+    impl<T: Scalar, const N: usize> Dot for Vector<T, N> {
+        type Scalar = T;
+
+        fn dot(self, rhs: Self) -> Self::Scalar {
+            let mut acc: Self::Scalar = T::default();
+            for i in 0..N {
+                acc += self.data[i] * rhs.data[i];
+            }
+            acc
+        }
+    }
+
+    // ////////////// COORDINATE NAME TRAITS /////////////////////
+    pub trait HasX {
+        type Scalar;
+        fn x(&self) -> &Self::Scalar;
+        fn x_mut(&mut self) -> &mut Self::Scalar;
+    }
+
+    pub trait HasY {
+        type Scalar;
+        fn y(&self) -> &Self::Scalar;
+        fn y_mut(&mut self) -> &mut Self::Scalar;
+    }
+
+    pub trait HasZ {
+        type Scalar;
+        fn z(&self) -> &Self::Scalar;
+        fn z_mut(&mut self) -> &mut Self::Scalar;
+    }
+
+    // ////////////// COORDINATE IMPLEMENTATIONS /////////////////////
+    impl<T: Scalar> HasX for Vector<T, 3> {
+        type Scalar = T;
+        fn x(&self) -> &Self::Scalar {
+            &self.data[0]
+        }
+        fn x_mut(&mut self) -> &mut Self::Scalar {
+            &mut self.data[0]
+        }
+    }
+
+    impl<T: Scalar> HasY for Vector<T, 3> {
+        type Scalar = T;
+        fn y(&self) -> &Self::Scalar {
+            &self.data[1]
+        }
+        fn y_mut(&mut self) -> &mut Self::Scalar {
+            &mut self.data[1]
+        }
+    }
+
+    impl<T: Scalar> HasZ for Vector<T, 3> {
+        type Scalar = T;
+        fn z(&self) -> &Self::Scalar {
+            &self.data[2]
+        }
+        fn z_mut(&mut self) -> &mut Self::Scalar {
+            &mut self.data[2]
+        }
     }
 }
